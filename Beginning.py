05f6ed3f822756897bc5f003ad38c32893b11816
hchao0804 <573442298@qq.com>
2018-11-16 03:48:54 +0800

    fisrt learning

---
+++ Beginning.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+chrome_options = Options()
+chrome_options.add_argument('--headless') #添加Chrome Headless
+browser = webdriver.Chrome(options=chrome_options)
+browser.get("https://www.baidu.com")
+print(browser.current_url)
+
+soup = BeautifulSoup("<p>Hello</p>","lxml")
+print(soup.p.string)
