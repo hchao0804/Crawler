7f5ae95b9eab38197b6db0ce9abbf6530a1d632b
hchao0804 <573442298@qq.com>
2018-11-26 17:25:23 +0800

    Maoyan TOP100

--- CrawelMaoyan.py
+++ CrawelMaoyan.py
@@ -1,20 +1,23 @@
 import requests
 import re
 import json
+import time
 
 def get_one_page(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
     }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.text
-    return None
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.exceptions.RequestException as e:
+        return None
 
 def parse_one_page(html):
     pattern = re.compile(
-        '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
-        re.S)
+        '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',re.S)
     items = re.findall(pattern, html)
     for item in items:
         yield {
@@ -28,14 +31,18 @@ def parse_one_page(html):
     return items
 
 def write_to_files(content):
-    with open('Maoyan.txt', 'a', encoding='utf-8') as f:
+    with open('Maoyan100.txt', 'a', encoding='utf-8') as f:
         f.write(json.dumps(content, ensure_ascii=False)+'\n')
 
-
-if __name__ == '__main__':
-    url = 'http://maoyan.com/board/4'
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
     html = get_one_page(url)
     items = parse_one_page(html)
     for item in items:
-        print(item)
+        # print(item)
         write_to_files(item)
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
